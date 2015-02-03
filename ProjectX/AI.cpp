6634143be2bcbb285a0472b6c33412b99960cb92
freakazoid1988 <luxaeterna88@gmail.com>
2015-02-03 21:01:26 +0100

    Continuazione classe AI

--- ProjectX/AI.cpp
+++ ProjectX/AI.cpp
@@ -8,11 +8,23 @@
 
 #include "AI.h"
 #include <string>
+#include "Scacchiera.cpp"
+#include <unordered_map>
+#include <cassert>
+#include <cmath>
+#include <iostream>
 
 class AI{
 private:
     std::string mossaFinale;
-    
+    const char CostiCattura[14] = { 0, 8, 12, 16, 24, 36, 100, 100, 100, 100, 100, 100, 100, 100 };
+    const int minColumn[10] = { 1, 1, 1, 1, 1, 1, 2, 3, 4, 5}; //da che colonna inizia la scacchiera per ogni riga compresa cornice
+    const int maxColumn[10] = { 5, 5, 6, 7, 8, 9, 9, 9, 9, 9}; //a che colonna finisce la scacchiera per ogni riga compresa cornice
+    char direzioni[6] = {1,2,3,4,5,6};//N,NO,O,S,SE,E
+    std::unordered_map<int, int> distance;
+    int dist;
+    const char white=2, black=3;
+    Scacchiera scacchiera;
     
     
 public:
