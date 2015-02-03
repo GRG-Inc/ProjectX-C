2fb5478defd2e5312e8aa5546665f106be0ad780
freakazoid1988 <luxaeterna88@gmail.com>
2015-02-03 20:32:04 +0100

    Iniziata classe AI

---
+++ ProjectX/AI.cpp
@@ -0,0 +1,21 @@
+//
+//  AI.cpp
+//  ProjectX
+//
+//  Created by Gaetano Belcastro on 03/02/15.
+//  Copyright (c) 2015 GRG. All rights reserved.
+//
+
+#include "AI.h"
+#include <string>
+
+class AI{
+private:
+    std::string mossaFinale;
+    
+    
+    
+public:
+    
+    
+};
---
+++ ProjectX/AI.h
@@ -0,0 +1,14 @@
+//
+//  AI.h
+//  ProjectX
+//
+//  Created by Gaetano Belcastro on 03/02/15.
+//  Copyright (c) 2015 GRG. All rights reserved.
+//
+
+#ifndef __ProjectX__AI__
+#define __ProjectX__AI__
+
+#include <stdio.h>
+
+#endif /* defined(__ProjectX__AI__) */
